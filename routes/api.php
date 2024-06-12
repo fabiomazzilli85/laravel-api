6e4383759a1b4f7403137cd4410aa72bf9175832
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-12 17:28:25 +0200

    Rimosso middleware.

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects', [ProjectController::class, 'index']);
