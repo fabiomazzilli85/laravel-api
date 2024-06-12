5cc1adfb98283806c0a92ad3eeda0ef7ee580e04
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-12 16:36:21 +0200

    Bug Fix Route auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts', [ProjectController::class, 'index']);
+Route::middleware('auth:sanctum')->get('/projects', [ProjectController::class, 'index']);
